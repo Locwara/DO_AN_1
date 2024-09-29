0ddef52f837f6a5170cf4ce32f5cffb3a446da3c
Locwara <lethanhloc2612004@gmail.com>
2024-09-29 07:58:42 +0700

    a

--- doan1/doan1/settings.py
+++ doan1/doan1/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'doan1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qlnv',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
